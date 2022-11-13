ecbcbd91d74f33a081eb90ba20c817c28efb8b7b
Newmoonhana <59281026+Newmoonhana@users.noreply.github.com>
2022-11-13 23:20:26 +0900

    new inputsystem 패키지 설치 & UI 추가 & 라인에 목표값과 합계 텍스트 추가
    +터치 시스템을 컴퓨터 모바일 다 되게 하는 법을 고민하다 연습 삼아 기존 인터페이스 방식이 아닌 뉴인풋시스템을 이용해볼까 싶어서 패키지 매니저에서 설치, 제작 중
    +모바일 해상도가 바뀌어도 가로 비율만 변하고 세로 비율은 변하지 않게 캔버스 설정 및 UI 오브젝트 추가
    +라인 프리팹에 목표값과 합계 텍스트  3d 오브젝트 추가

--- Assets/MyAssets/Scripts/Line/Core/NumberLine.cs
+++ Assets/MyAssets/Scripts/Line/Core/NumberLine.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NumberLine : MonoBehaviour
 {
+    internal uint goalCal;
+    uint cal;
     [SerializeField] internal SpriteRenderer sr;
     [SerializeField] internal SpriteRenderer bg_sr;
+    [SerializeField] internal RectTransform goalCal_tns;
+    [SerializeField] internal TextMeshPro goalCal_txt;
+    [SerializeField] internal RectTransform cal_tns;
+    [SerializeField] internal TextMeshPro cal_txt;
 
     public void ChangeSize(Vector2 _size)
     {
         sr.size = _size;
         bg_sr.size = _size;
+        Vector2 sizeDelta = Vector2.one;
+        sizeDelta.x = _size.x;
+        goalCal_tns.sizeDelta = sizeDelta * 10;
+        cal_tns.sizeDelta = sizeDelta * 10;
     }
 }
